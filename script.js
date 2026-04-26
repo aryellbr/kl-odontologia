92bf58d89005ffc2f2efc5b0979b6f4389ea8542
Pedro Aryel Oliveira <ia.pedronunes@gmail.com>
2026-04-26 17:50:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('.btn');
+
+  btn.addEventListener('click', () => {
+    alert('Você será redirecionado para o WhatsApp para agendar sua consulta.');
+  });
+});
